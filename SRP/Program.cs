82ff8e4d5cbd3371f6bb71e3b22a947e4ea08597
LucianoCamposLima <lucianocamposlima@yahoo.com.br>
2021-06-17 18:03:25 -0300

    first commit

---
+++ SRP/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SRP
+{
+    class Program
+    {
+        // Problema: Métodos na classe principal que não tem a ver com o assunto principal.
+        // Solução: Criar uma classe Cliente e uma classe Notifacação.
+        // Não deixar as classes engessadas. Refatorar e criar classes para cada assunto.
+
+        //static void Main(string[] args)
+        //{
+        //}
+
+        //static void CreateClient()
+        //{ }
+
+        //static void ReadClient()
+        //{ }
+
+        //static void UpdateClient()
+        //{ }
+
+        //static void DeleteClient()
+        //{ }
+
+        //static void NotifyClient()
+        //{ }
+
+
+    }
+}
---
+++ SRP/Utils/Client.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SRP.Utils
+{
+    public class Client
+    {
+        public string Nome { get; set; }
+
+        public void CreateClient()
+        { }
+
+        public void ReadClient()
+        { }
+
+        public void UpdateClient()
+        { }
+
+        public void DeleteClient()
+        { }
+
+    }
+}
